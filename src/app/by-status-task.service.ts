73c7313dd0f37f8ea79be23c74f8b4632467cafe
Pehlow <poissonnet.jules@gmail.com>
2023-08-28 17:11:53 +0200

    k;k;l

--- src/app/by-status-task.service.ts
+++ src/app/by-status-task.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {CRUDTaskListService} from "@/app/c-r-u-d-task-list.service";
+import {TaskState} from "@/app/model/Task";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ByStatusTaskService {
+export class ByStatusTaskService extends CRUDTaskListService {
 
-  constructor() { }
+  constructor() {
+    super();
+  }
+
+  getByStatus(status: TaskState) {
+    return this.getTaskList().tasks.filter(task => task.etat === status);
+  }
 }
--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {CRUDTaskListService} from "@/app/c-r-u-d-task-list.service";
+import {ByStatusTaskService} from "@/app/by-status-task.service";
+import {TaskState} from "@/app/model/Task";
 
 @Component({
   selector: 'app-task-list',
@@ -8,9 +10,11 @@ import {CRUDTaskListService} from "@/app/c-r-u-d-task-list.service";
 })
 export class TaskListComponent {
 
-  constructor(private taskListService: CRUDTaskListService) {
+
+  constructor(private taskListService: CRUDTaskListService, private byStatusTaskService: ByStatusTaskService) {
   }
 
   taskList = this.taskListService.getTaskList();
+  archivedTaskList = this.byStatusTaskService.getByStatus(TaskState.TERMINE);
 
 }
