e6131744631178a82f22ae9ad04dd736b60d6e59
Pehlow <poissonnet.jules@gmail.com>
2023-08-29 14:52:27 +0200

    error required

--- src/app/add-task-form/add-task-form.module.ts
+++ src/app/add-task-form/add-task-form.module.ts
@@ -3,16 +3,19 @@ import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { ErrorMinLengthFieldFormComponent } from './error-min-length-field-form/error-min-length-field-form.component';
 import { ErrorMaxLengthFieldFormComponent } from './error-max-length-field-form/error-max-length-field-form.component';
+import { ErrorRequiredFieldFormComponent } from './error-required-field-form/error-required-field-form.component';
 
 
 @NgModule({
   declarations: [
     ErrorMinLengthFieldFormComponent,
-    ErrorMaxLengthFieldFormComponent
+    ErrorMaxLengthFieldFormComponent,
+    ErrorRequiredFieldFormComponent
   ],
   exports: [
     ErrorMinLengthFieldFormComponent,
-    ErrorMaxLengthFieldFormComponent
+    ErrorMaxLengthFieldFormComponent,
+    ErrorRequiredFieldFormComponent
   ],
   imports: [
     CommonModule,
